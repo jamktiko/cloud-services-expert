7c9603477c3ff22801b50f9eefa352c6d3a5a7de
sultapro <124219518+sultapro@users.noreply.github.com>
2023-05-20 17:52:24 +0300

    Updated AWSAIandMLInTheCloud folder

---
+++ AWSAIandMLInTheCloud/rekognition_2/2-createCollectionFacesAddFace.js
@@ -0,0 +1,36 @@
+import { RekognitionClient, CreateCollectionCommand, IndexFacesCommand } from "@aws-sdk/client-rekognition";
+import fs from "fs";
+
+// Set the AWS Region
+const REGION = "us-east-1"; // Replace with your desired region
+
+// Create a new Rekognition client object and configure it
+const rekogClient = new RekognitionClient({ region: REGION });
+
+// Specify the name of the collection
+const collectionName = "faces";
+
+// Read the image data from a file
+const image = fs.readFileSync("image.jpeg");
+
+// Create the Rekognition collection
+const createCollection = async () => {
+  try {
+    const createCollectionCommand = new CreateCollectionCommand({ CollectionId: collectionName });
+    const response = await rekogClient.send(createCollectionCommand);
+    console.log("Collection created:", response);
+
+    // Add the image to the collection
+    const indexFacesCommand = new IndexFacesCommand({
+      CollectionId: collectionName,
+      Image: { Bytes: image },
+      ExternalImageId: "image.jpg",
+    });
+    const indexFacesResponse = await rekogClient.send(indexFacesCommand);
+    console.log("Face indexed:", indexFacesResponse);
+  } catch (error) {
+    console.log("Error creating collection:", error);
+  }
+};
+
+createCollection();
